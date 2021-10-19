abdd4465f716a6f57c27f15117b1b151431f7488
Alexious Yong <alexiousyong@outlook.com>
2021-10-19 15:51:34 +0800

    1. Adding Authorization in controllers 2. Registration module

--- Controllers/Authentictation/RegisterController.cs
+++ Controllers/Authentictation/RegisterController.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.UI.WebControls;
+﻿using System.Web.Mvc;
+using aspMVCDemo.HelperServices;
+using aspMVCDemo.Manager;
 using aspMVCDemo.Models.Account;
 
 namespace aspMVCDemo.Controllers.Authentictation
 {
     public class RegisterController : Controller, IRegisterController
     {
+        private readonly AccountManager _accountManager;
+
+        public RegisterController(AccountManager accountManager)
+        {
+            _accountManager = accountManager;
+        }
+
         // GET: Register
         public ActionResult Index()
         {
@@ -18,10 +22,31 @@ namespace aspMVCDemo.Controllers.Authentictation
         }
 
         [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
         public ActionResult Register(CreateAccountDto input)
         {
+            bool validPassword = Authentication.PasswordConfirmation(input.Password, input.ConfirmPassword);
+            
+            if (ModelState.IsValid && validPassword)
+            {
+                var acc = _accountManager.CreateAccount(input);
 
-            return RedirectToAction("Index","Login");
+                if (acc.Result == null)
+                {
+                    //Registration failed
+                    ViewBag.Error = "Unsuccessful registration.";
+                    return RedirectToAction("Index","Register");
+                }
+                
+                // Successful registration
+                ViewBag.Error = "Successful registration. Click \"Ok\" and Go to Login Page...";
+                return RedirectToAction("Index","Login");
+            }
+
+            //Registration failed
+            ViewBag.Error = "Unsuccessful registration.";
+            return View("Register");
         }
     }
 }
