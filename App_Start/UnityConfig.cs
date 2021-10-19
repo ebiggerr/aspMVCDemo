b303a24cee5533c2c70545bd891b43f31b6ddcb5
Alexious Yong <alexiousyong@outlook.com>
2021-10-19 14:18:29 +0800

    Type registration
    1.Type is registered Unity creates a contract (a registration) consisting of a Type, a Name, and instructions on how this contract should be fulfilled when requested.

--- App_Start/UnityConfig.cs
+++ App_Start/UnityConfig.cs
@@ -1,5 +1,6 @@
 using System.Web.Mvc;
 using aspMVCDemo.Controllers.Accounts;
+using aspMVCDemo.Controllers.Authentictation;
 using aspMVCDemo.Manager;
 using Unity;
 using Unity.Mvc5;
@@ -18,6 +19,9 @@ namespace aspMVCDemo
             // e.g. container.RegisterType<ITestService, TestService>();
             container.RegisterType<IAccountManager, AccountManager>();
             container.RegisterType<IAccountController, AccountController>("Account");
+            
+            container.RegisterType<ILoginController, LoginController>("Login");
+            container.RegisterType<IRegisterController, RegisterController>("Register");
             
             DependencyResolver.SetResolver(new UnityDependencyResolver(container));
         }
