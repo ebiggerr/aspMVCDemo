f6cc6d1275ba20362615a35b2a87ae630eb0d808
Alexious Yong <alexiousyong@outlook.com>
2021-10-19 15:49:31 +0800

    Buidling block of Authentication
    1. Added Bcrypt.Net package in the project
    2. Implemented password hashing.

--- HelperServices/Authentication.cs
+++ HelperServices/Authentication.cs
@@ -4,13 +4,13 @@
     {
         public static string Hashing(string password)
         {
-
-            return "dsdadasda";
+            //anything above 10 would be recommended, too much while can kill the processor and it is impractical
+            return BCrypt.Net.BCrypt.HashPassword(password, 14);
         }
 
         public static bool ValidatePassword(string dBPassword, string inputPassword)
         {
-            return true;
+            return BCrypt.Net.BCrypt.Verify(inputPassword, dBPassword);
         }
 
         public static bool PasswordConfirmation(string password, string confirmPassword)
